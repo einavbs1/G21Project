448413dd3ba7249f63dc47eae6aa9a56a88e0434
Einavbs1 <einav.momi.ben.shsus@e.braude.ac.il>
2024-12-27 16:59:09 +0200

    MainServerGUI- 27/12/2024 - 17:00

---
+++ G21_Server_Prototype_HW2/src/Server/EchoServer.java
@@ -0,0 +1,129 @@
+package Server;
+
+import java.util.HashMap;
+import java.util.List;
+import common.UserSelect;
+import gui.ClientConnectionStatusController;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ocsf.server.*;
+/*
+ * This class is defining as a server
+ */
+public class EchoServer extends AbstractServer {
+	int connectionPort;
+	public static HashMap<String, String> clientsstatusconnections = new HashMap<String, String>();
+	public static String popUpString;
+	public static Scene tableScene; 
+	public static Stage tableStage; 
+	public static ClientConnectionStatusController tableController;
+	
+
+	/** Constructs an instance of the echo server.
+	 * @param port - The port number to connect on.
+	 */
+	public EchoServer(int port) {
+		super(port);
+		connectionPort = port;
+	}
+
+
+	/**
+	 * This method handles any messages received from the client.
+	 *
+	 * @param msg    The message received from the client.
+	 * @param client The connection from which the message originated.
+	 */
+	public void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		int flag = 0;
+		String clientIp = client.getInetAddress().getHostAddress();
+		String clientPCName = client.getInetAddress().getHostName();
+		
+		HashMap<String, String> infoFromUser = (HashMap<String, String>) msg;
+		String menuChoiceString = (infoFromUser.keySet().iterator().next());
+		
+		UserSelect x = UserSelect.getSelectionFromEnumName(menuChoiceString);
+		
+		switch (x) {
+		//This case is getting the table from the SQL and sending to the client
+		case ShowAllSubscribers:
+			List<String> TheTable = mysqlConnection.GetSubscriberTable();
+			this.sendToAllClients(TheTable);
+			flag++;
+			break;
+		//This case is getting the information to change from the user and saving in DB.
+		case UpdatePhoneNumber:
+			String idNphone[] = infoFromUser.get(menuChoiceString).split(" ");
+			boolean succ1 = mysqlConnection.updatephone(Integer.parseInt(idNphone[0]), idNphone[1]);
+			if (succ1) {
+				this.sendToAllClients("Updated");
+			} else {
+				this.sendToAllClients("Error");
+			}
+
+			flag++;
+			break;
+		//This case is getting the information to change from the user and saving in DB.
+		case UpdateEmailAddress:
+			String idNemail[] = infoFromUser.get(menuChoiceString).split(" ");
+			boolean succ2 = mysqlConnection.updatemail(Integer.parseInt(idNemail[0]), idNemail[1]);
+			if (succ2) {
+				this.sendToAllClients("Updated");
+			} else {
+				this.sendToAllClients("Error");
+			}
+			flag++;
+			break;
+		//This case is loading the requested ID from the DB and sending to the client.
+		case LoadSubscriber:
+			String RequestedID = mysqlConnection.Load(Integer.parseInt(infoFromUser.get(menuChoiceString)));
+			this.sendToAllClients(RequestedID);
+			flag++;
+			break;
+		//This case is Showing the client that connect to the server and showing it on the table GUI and shows a message.
+		case Connected:
+			String onlineStatuString = (clientIp + ", " + clientPCName + ", Connected");
+			popUpString = "Client from IP: " + clientIp + ", HostName: " + clientPCName + ", Status: Connected";
+			//System.out.println("Client from IP: " + clientIp + ", HostName: " + clientPCName + ", Status: Connected");
+			if(clientsstatusconnections.containsKey(clientIp)) {
+				clientsstatusconnections.remove(clientIp);
+			}
+			clientsstatusconnections.put(clientIp, onlineStatuString);
+			tableController.setconnection(popUpString);
+			this.sendToAllClients("Connected");
+			flag++;
+			break;
+		//This case is Showing the client that disconnected to the server and showing it on the table GUI and shows a message.
+		case Disconnect:
+			String offlineStatuString = (clientIp + ", " + clientPCName + ", Disconnected");
+			popUpString = "Client from IP: " + clientIp + ", HostName: " + clientPCName + ", Status: Disconnected";
+			//System.out.println("Client from IP: " + clientIp + ", HostName: " + clientPCName + ", Status: Disconnected");
+			clientsstatusconnections.remove(clientIp);
+			clientsstatusconnections.put(clientIp, offlineStatuString);
+			tableController.setconnection(popUpString);
+			this.sendToAllClients("Disconnected");
+			flag++;
+			break;
+		//error with the userselect action.
+		default:
+			System.out.println("Error with the choise? = " + menuChoiceString);
+			break;
+
+		}
+		if (flag != 1) {
+			this.sendToAllClients("Error");
+		}
+	}
+
+
+	/**
+	 * This method overrides the one in the superclass. Called when the server stops
+	 * listening for connections.
+	 */
+	protected void serverStopped() {
+		System.out.println("Server has stopped listening for connections.");
+	}
+
+	
+}
+//End of EchoServer class
---
+++ G21_Server_Prototype_HW2/src/Server/mysqlConnection.java
@@ -0,0 +1,141 @@
+package Server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * This class is connect to mySQL DB for G21-prototype server. 
+ */
+public class mysqlConnection {
+
+	public static Connection conn;
+
+	/*
+	 * This method is connecting to out G21-prototype server
+	 */
+	public static String connectToDB() {
+		String ret ="";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			ret = ret + ("Driver definition succeed");
+
+		} catch (Exception ex) {
+			/* handle the error */
+			ret = ret + ("Driver definition failed");
+		}
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/g21_schema?serverTimezone=IST", "root",
+					"Aa123456");
+			ret = ret + ("SQL connection succeed");
+		} catch (SQLException ex) {/* handle any errors */
+			
+			ret = ret + ("SQLException: " + ex.getMessage());
+			ret = ret + ("\nSQLState: " + ex.getSQLState());
+			ret = ret + ("\nVendorError: " + ex.getErrorCode());
+		}
+		return ret;
+		
+	}
+
+	/**
+	 * This method is getting information to change in the DB 
+	 * @param id		- subscriber ID (PK)
+	 * @param phonenum	- Phone number to change to the subscriber
+	 * @return True if success to save the changes
+	 */
+	public static boolean updatephone(int id, String phonenum) {
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("UPDATE subscriber SET subscriber_phone_number = \"" + phonenum
+					+ "\" WHERE  subscriber_id = \"" + id + "\"");
+			stmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * This method is getting information to change in the DB 
+	 * @param id		- subscriber ID (PK)
+	 * @param email		- Email address to change to the subscriber
+	 * @return True if success to save the changes
+	 */
+	public static boolean updatemail(int id, String email) {
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement(
+					"UPDATE subscriber SET subscriber_email = \"" + email + "\" WHERE  subscriber_id = \"" + id + "\"");
+			stmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * This method is getting ID and returning String of his data
+	 * @param idtoload	- ID that client ask his details
+	 * @return String of this subscriber
+	 */
+	public static String Load(int idtoload) {
+		String query = "SELECT * FROM subscriber WHERE  subscriber_id = \"" + idtoload + "\"";
+		String subscriberData = new String("Empty");
+		try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				int id = rs.getInt("subscriber_id");
+				String name = rs.getString("subscriber_name");
+				int history = rs.getInt("detailed_subscription_history");
+				String phoneNumber = rs.getString("subscriber_phone_number");
+				String email = rs.getString("subscriber_email");
+
+				// Create a formatted string with the subscriber's information
+				subscriberData = id + ", " + name + ", " + history + ", " + phoneNumber + ", " + email;
+				return subscriberData;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return subscriberData;
+
+	}
+
+	
+	/**
+	 * This method is returning the list of subscribers from the DB to the client
+	 * @return List of the subscribers
+	 */
+	public static List<String> GetSubscriberTable() {
+		List<String> subscribers = new ArrayList<>();
+		String query = "SELECT * FROM subscriber";
+
+		try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				int id = rs.getInt("subscriber_id");
+				String name = rs.getString("subscriber_name");
+				int history = rs.getInt("detailed_subscription_history");
+				String phoneNumber = rs.getString("subscriber_phone_number");
+				String email = rs.getString("subscriber_email");
+
+				// Create a formatted string with the subscriber's information
+				String subscriberData = id + ", " + name + ", " + history + ", " + phoneNumber + ", " + email;
+
+				// Add the formatted string to the list
+				subscribers.add(subscriberData);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return subscribers;
+	}
+
+}
